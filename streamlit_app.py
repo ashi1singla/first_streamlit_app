66d6ae135b643df2217d6c8c603567d5de4ccb20
Ashima <31312601+ashi1singla@users.noreply.github.com>
2022-11-01 13:43:05 +0000

    error fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('New Diner is here')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text#('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text ('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
