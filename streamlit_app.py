7b7a42874c13c4235643741db1829744b2e004be
Ashima <31312601+ashi1singla@users.noreply.github.com>
2022-11-08 11:43:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('New Diner is here')
 streamlit.header('Breakfast Menu')
@@ -6,3 +7,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.title('🍌🥭Build your own smoothie🥝🍇')
+
+
